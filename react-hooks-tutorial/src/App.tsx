d75a9aa516b8570c18f1ede3ae26f79dd0e5ddcf
ehermance <ellen.diep@gmail.com>
2025-08-28 17:31:39 -0500

    useState and useEffect first commit

---
+++ react-hooks-tutorial/src/App.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { useForm } from "./hooks/useForm";
+import { useFetch } from "./hooks/useFetch";
+import "./App.css";
+
+const App = () => {
+  const [values, handleChange] = useForm({
+    email: "",
+    password: "",
+    firstName: "",
+  });
+  const [count, setCount] = useState(() =>
+    JSON.parse(localStorage.getItem("count"))
+  );
+  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
+
+  return (
+    <>
+      <q>{!data ? "loading..." : data}</q>
+      <div>
+        count:
+        <button onClick={() => setCount((c) => c - 1)}>-</button>
+        {count}
+        <button onClick={() => setCount((c) => c + 1)}>+</button>
+      </div>
+
+      <div>
+        <input name="email" value={values.email} onChange={handleChange} />
+        <input
+          name="firstName"
+          placeholder="first name"
+          value={values.firstName}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+      </div>
+    </>
+  );
+};
+
+export default App;
